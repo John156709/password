1fd4892627c0bdb1a5b3489f7a89321f5a7fa0fd
John156709 <johnjohn15670@gmail.com>
2025-02-01 13:11:06 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function generatePassword() {
+    let length = 12;
+    let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        let randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    document.getElementById("password").textContent = password;
+}
